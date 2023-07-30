e68d15fc716adb3d40abcc02f7a45d4f75040218
AngerOverApathy <violasian@gmail.com>
2023-07-30 14:02:11 -0700

    more styling

--- business-card/src/components/MainContent.jsx
+++ business-card/src/components/MainContent.jsx
@@ -3,10 +3,15 @@ import React from "react";
 function MainContent() {    
     return (
         <div className="main-content">
-            <h1>Hi, I'm <strong>Chris</strong>.</h1>
-            <p>I'm a full-stack web developer with a passion for learning and a love of problem solving.</p>
-            <p>My background in the arts has given me a unique perspective on the world, and I'm excited to bring that perspective to the world of web development.</p>
-            <p>Take a look around and see what I've been up to!</p>
+            <div className="title">
+                <h1>Jessica Versoza</h1>
+                <h3>Software Develoer</h3>
+                <p>versoza-tech.netlify.com</p>
+            </div>
+            <button className="email">Email</button>
+            <h3>About</h3>
+
+            <h3>Interests</h3>
         </div>
     )
 }
