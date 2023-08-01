005ecd2340c762ba2f2f7a5e4f91d27ebe978c95
AngerOverApathy <violasian@gmail.com>
2023-08-01 11:03:50 -0700

    more styling

--- business-card/src/main.jsx
+++ business-card/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='container'>
     <Header />
     <MainContent />
+    <Footer />
   </div>,
 )
