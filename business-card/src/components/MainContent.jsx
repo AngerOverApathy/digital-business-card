5f90bb0cc8acc4f3bce4663962b4d6f4f9dd455e
AngerOverApathy <violasian@gmail.com>
2023-08-01 10:40:21 -0700

    styling added

---
+++ business-card/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MainContent() {    
+    return (
+        <div className="main-content">
+            <h1>Hi, I'm <strong>Chris</strong>.</h1>
+            <p>I'm a full-stack web developer with a passion for learning and a love of problem solving.</p>
+            <p>My background in the arts has given me a unique perspective on the world, and I'm excited to bring that perspective to the world of web development.</p>
+            <p>Take a look around and see what I've been up to!</p>
+        </div>
+    )
+}
+
+export default MainContent;
